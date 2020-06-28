a7ba9411c713ec6751cc9878e9859c5f8e06a326
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-28 19:43:34 -0400

    started building app.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+
+import React, { useState, useEffect } from 'react';
+import CharacterCard from './CharacterCard'
+
+export default function Character() {
+
+
+    return (
+        <div>
+            <CharacterCard />
+        </div>
+    )
+}
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CharacterCard = ([]) => {
+
+    return (
+        <div>
+            <h2>Name: </h2>
+            <h3>Height: Mass</h3>
+            <p>hair_color, skin_color, eye_color, gender</p>
+        </div>
+    )
+}
+
+export default CharacterCard;
