6d6e029c4b0a7c920623ae52f42777712940f56e
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-28 20:13:27 -0400

    mapped and rendered character cards.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const CharacterCard = ([]) => {
+const CharacterCard = (props) => {
 
     return (
         <div>
-            <h2>Name: </h2>
-            <h3>Height: Mass</h3>
-            <p>hair_color, skin_color, eye_color, gender</p>
+            <h2>Name: {props.name}</h2>
+            <h3>Height: {props.height} Mass: {props.mass}</h3>
+            <p>hair_color - {props.hair_color} skin_color {props.skin_color} eye_color {props.eye_color} gender {props.gender}</p>
         </div>
     )
 }
