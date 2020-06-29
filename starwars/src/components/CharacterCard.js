c944b50dff8af7538be9953077ff673537ea8b27
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-28 20:56:15 -0400

    added more styling.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Hair = styled.p`
+font-size: 1.25rem;
+text-decoration: underline;
+background-color: white;
+margin: 0 40%;
+border-radius: 60px;
+`
+const Skin = styled.p`
+font-size: 1.25rem;
+text-decoration: underline;
+background-color: white;
+margin: 0 40%;
+border-radius: 60px;
+`
+
+const Eye = styled.p`
+font-size: 1.25rem;
+text-decoration: underline;
+background-color: white;
+margin: 0 40%;
+border-radius: 60px;
+`
+
+const Gender = styled.p`
+  font-size: 1.25rem;
+  text-decoration: underline;
+  background-color: white;
+  margin: 0 40%;
+  border-radius: 60px;
+`
+
+const Name = styled.h2`
+font-size: 2rem;
+text-decoration: underline red dashed;
+`
+const Height = styled.h3`
+font-size: 1.5rem;
+border: 2px white;
+border-radius: 25px;
+background-color: lime;
+margin: 2% 30%;
+`
+
+
 
 const CharacterCard = (props) => {
 
     return (
         <div>
-            <h2>Name: {props.name}</h2>
-            <h3>Height: {props.height} Mass: {props.mass}</h3>
-            <p>hair_color - {props.hair_color} skin_color {props.skin_color} eye_color {props.eye_color} gender {props.gender}</p>
+            <Name>Name: {props.name}</Name>
+            <Height>Height: {props.height}cm,  Mass: {props.mass}kgs</Height>
+            <Hair>hair color - {props.hair_color}</Hair>
+            <Skin>skin color -  {props.skin_color}</Skin>
+            <Eye>eye color -  {props.eye_color}</Eye>
+            <Gender>gender: {props.gender}</Gender>
         </div>
     )
 }
